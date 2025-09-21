4a173a78a69b024d8410a8a7b06fc15d22a2a074
Edward J <edd.f.johnson.work@gmail.com>
2025-09-21 13:11:18 -0400

    add/config tailwind and prettier formatting for frontend.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 // frontend/vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8080'
-    }
-  }
-})
+      '/api': 'http://localhost:8080',
+    },
+  },
+});
