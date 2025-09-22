25dc12061d0ffea8a1eb8d9cf4a7ee64adecee4d
Edward J <edd.f.johnson.work@gmail.com>
2025-09-22 16:14:50 -0400

    impl: initial business layer UserAccount repo interface defined. basic crud added, save/delete added with general attribute retrieval methods for email, role, id, and retrieve all

--- core/src/main/java/com/ddjproj/revature/repository/UserAccountRepository.java
+++ core/src/main/java/com/ddjproj/revature/repository/UserAccountRepository.java
@@ -1,4 +1,20 @@
 package com.ddjproj.revature.repository;
 
+import com.ddjproj.revature.domain.entity.Role;
+import com.ddjproj.revature.domain.entity.UserAccount;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UserAccountRepository {
+    Optional<UserAccount> findByEmail(String email);
+    boolean existsByEmail(String email);
+    List<UserAccount> findByRole(String role);
+    boolean existsByRole(Role targetRole);
+
+    UserAccount save(UserAccount userAccount);
+    Optional<UserAccount> findById(Long id);
+    List<UserAccount> findAll();
+    void deleteById(Long id);
+
 }
