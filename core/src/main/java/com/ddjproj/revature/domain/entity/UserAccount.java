96ebb099564f9062d359add053353b2a31e9d27c
Edward J <edd.f.johnson.work@gmail.com>
2025-09-22 16:12:08 -0400

    impl: initial UserAccount definition created. accountId, email, password, role, and roleId attributes defined. Class and id attribute annotations added

--- core/src/main/java/com/ddjproj/revature/domain/entity/UserAccount.java
+++ core/src/main/java/com/ddjproj/revature/domain/entity/UserAccount.java
@@ -1,4 +1,34 @@
 package com.ddjproj.revature.domain.entity;
 
+import com.ddjproj.revature.domain.enums.Roles;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="user_account")
+@Getter
+@Setter
 public class UserAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "user_id", nullable = false)
+    private Long userAccountId;
+
+    private String email;
+
+    private String passwordHash;
+
+    // default role assigned to a newly created userAccount
+    private Roles role = Roles.RESTRICTED;
+
+    // ex: employeeId = "EMP" + userAccountId;
+    // ex: managerId = "MAN" + userAccountId;
+    private String roleId;
+
+    // are we adding a created date? if so
+    // private LocalDateTime creationDate = LocalDateTime.now();
+
+
 }
