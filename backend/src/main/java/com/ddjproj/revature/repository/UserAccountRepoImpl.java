1ea13a5780d8135c092ad60bbfe42b95c17e2f78
Edward J <edd.f.johnson.work@gmail.com>
2025-09-22 16:16:30 -0400

    impl: initial network layer UserAccount repo implementation defined. auto implemented methods provided in interface, added attribute for later definition of entityMapper for dao's, and JpaUserAccountRepository

--- backend/src/main/java/com/ddjproj/revature/repository/UserAccountRepoImpl.java
+++ backend/src/main/java/com/ddjproj/revature/repository/UserAccountRepoImpl.java
@@ -1,4 +1,56 @@
 package com.ddjproj.revature.repository;
 
-public class UserAccountRepoImpl {
+import com.ddjproj.revature.domain.entity.Role;
+import com.ddjproj.revature.domain.entity.UserAccount;
+import com.ddjproj.revature.dto.EntityMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class UserAccountRepoImpl implements UserAccountRepository{
+    private final JpaUserAccountRepository jpaUserAccountRepository;
+    private final EntityMapper entityMapper;
+
+
+    @Override
+    public Optional<UserAccount> findByEmail(String email) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return false;
+    }
+
+    @Override
+    public List<UserAccount> findByRole(String role) {
+        return List.of();
+    }
+
+    @Override
+    public boolean existsByRole(Role targetRole) {
+        return false;
+    }
+
+    @Override
+    public UserAccount save(UserAccount userAccount) {
+        return null;
+    }
+
+    @Override
+    public Optional<UserAccount> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<UserAccount> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
 }
