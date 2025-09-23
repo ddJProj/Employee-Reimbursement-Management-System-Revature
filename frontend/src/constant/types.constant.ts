9c9dca184c778696ede06b4c677e335f35869d70
Edward J <edd.f.johnson.work@gmail.com>
2025-09-23 15:47:22 -0400

    impl: UserAccount interface, permissions & roletype enums defined

--- frontend/src/constant/types.constant.ts
+++ frontend/src/constant/types.constant.ts
@@ -9,12 +9,14 @@ export enum RoleType {
 export interface UserAccount {
 	id: number, // userAccount Id
 	email: ,
-	// name: ?,
+	// name: ,?
 	role: RoleType,
-	// permissions: []?, add here? or destructure from role?
+	permissions: [], // add here? or destructure from role?
 }
 
 
 export enum Permissions {
-
+// TODO: INSERT THE FULL LIST OF PERMISSION HERE WHEN FINISHED
+	// follow this format:
+	// REQUEST_REIMBURSEMENT = 'REQUEST_REIMBURSEMENT',
 }
