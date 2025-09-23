3ee42e0a934596b73c57c5ad85192f08f1e4cda2
Edward J <edd.f.johnson.work@gmail.com>
2025-09-23 19:53:09 -0400

    refactor: fixed references to Roles enum in method args

--- core/src/main/java/com/ddjproj/revature/repository/UserAccountRepository.java
+++ core/src/main/java/com/ddjproj/revature/repository/UserAccountRepository.java
@@ -1,7 +1,7 @@
 package com.ddjproj.revature.repository;
 
-import com.ddjproj.revature.domain.entity.Role;
 import com.ddjproj.revature.domain.entity.UserAccount;
+import com.ddjproj.revature.domain.enums.Roles;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,7 +10,7 @@ public interface UserAccountRepository {
     Optional<UserAccount> findByEmail(String email);
     boolean existsByEmail(String email);
     List<UserAccount> findByRole(String role);
-    boolean existsByRole(Role targetRole);
+    boolean existsByRole(Roles targetRole);
 
     UserAccount save(UserAccount userAccount);
     Optional<UserAccount> findById(Long id);
