e8e7b8df5cb9ed1fc979e3b22f308dc9561cb777
Edward J <edd.f.johnson.work@gmail.com>
2025-09-23 19:37:17 -0400

    refactor: reworked Roles enum to include permissions related logic in each Role definition

--- core/src/main/java/com/ddjproj/revature/domain/enums/Roles.java
+++ core/src/main/java/com/ddjproj/revature/domain/enums/Roles.java
@@ -1,8 +1,39 @@
 package com.ddjproj.revature.domain.enums;
 
+import java.util.EnumSet;
+import java.util.Set;
+
+import static com.ddjproj.revature.domain.enums.Permissions.*;
+
 public enum Roles {
-    GUEST,
-    RESTRICTED,
-    EMPLOYEE,
-    MANAGER
+    GUEST(EnumSet.of(
+        Permissions.CREATE_ACCOUNT
+    )),
+    RESTRICTED(EnumSet.of(
+        Permissions.REQUEST_EMPLOYEE_ACCOUNT,
+        Permissions.LOGOUT
+    )),
+    EMPLOYEE(EnumSet.of(
+        Permissions.CREATE_REIMBURSEMENT_REQUEST,
+        Permissions.VIEW_SUBMITTED_REIMBURSEMENT_REQUESTS,
+        Permissions.LOGOUT
+    )),
+    MANAGER(EnumSet.of(
+        Permissions.VIEW_SUBMITTED_REIMBURSEMENT_REQUESTS,
+        Permissions.VIEW_ALL_REIMBURSEMENT_REQUESTS,
+        Permissions.EDIT_USER_ROLE,
+        Permissions.UPGRADE_ACCOUNT_ROLE,
+        Permissions.DELETE_USER,
+        Permissions.LOGOUT
+    )
+);
+private final Set<Permissions> permissions;
+
+Roles(Set<Permissions> permissions){
+    this.permissions = EnumSet.copyOf(permissions);
 }
+
+public Set<Permissions> getPermissions() {
+    return EnumSet.copyOf(permissions);
+}
+    }
