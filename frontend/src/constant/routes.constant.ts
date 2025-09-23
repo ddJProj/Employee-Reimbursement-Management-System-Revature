8e80dcfd6ebfcc0bf59b1af63df0670561451d14
Edward J <edd.f.johnson.work@gmail.com>
2025-09-23 15:46:25 -0400

    impl: base app routes & router config defined as constants

---
+++ frontend/src/constant/routes.constant.ts
@@ -0,0 +1,50 @@
+// src/constants/routes.constants.ts
+//
+
+export const ROUTES = {
+	LOGIN: "/auth/login",
+	REGISTER: "/auth/register",
+	DASHBOARD: "/dashboard",
+	MANAGER: "/dashboard/manager",
+	EMPLOYEE: "/dashboard/employee",
+	RESTRICTED: "dashboard/restricted",
+	GUEST: "/auth/login"
+};
+
+export const ROUTER_CONFIG = {
+	{
+	path: ROUTES.DASHBOARD ,
+	inProtected: false,
+	},
+	{
+	path: ROUTES.LOGIN ,
+	inProtected: false,
+	},
+	{
+	path: ROUTES.REGISTER ,
+	inProtected: false,
+	},	
+	{
+	path: ROUTES.GUEST ,
+	inProtected: false,
+	},
+	{
+	path: ROUTES.MANAGER ,
+	inProtected: true,
+	},
+	{
+	path: ROUTES.EMPLOYEE,
+	inProtected: true,
+	},
+	{
+	path: ROUTES.RESTRICTED ,
+	inProtected: true,
+	},
+
+
+
+
+
+
+
+}
