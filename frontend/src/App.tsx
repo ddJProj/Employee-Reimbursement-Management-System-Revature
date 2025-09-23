732bed816b1ce52464b160ab1dc786d8bac21a6d
Edward J <edd.f.johnson.work@gmail.com>
2025-09-23 11:28:06 -0400

    impl: added header/footer to main app div block for rendering on page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
+import Header from './components/pages/layout/Header.jsx'
+import Footer from './components/pages/layout/Footer.jsx'
 
 function App() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <Header />
+
       <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
         Hello Tailwind!
       </button>
+
+      <Footer />
     </div>
   );
 }
