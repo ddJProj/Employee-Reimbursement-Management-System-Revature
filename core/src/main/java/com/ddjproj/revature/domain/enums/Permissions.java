85600777852cc7ad5518cd2378d56969b053e95b
Edward J <edd.f.johnson.work@gmail.com>
2025-09-24 11:15:57 -0400

    refactor: added additional permissions to list, updated descriptions of permissions

--- core/src/main/java/com/ddjproj/revature/domain/enums/Permissions.java
+++ core/src/main/java/com/ddjproj/revature/domain/enums/Permissions.java
@@ -17,12 +17,13 @@ public enum Permissions {
 
     /**
      *
-     * Admin level:
+     * Manager level:
      */
-    EDIT_USER_ROLE("Edits the Role attribute value of a UserAccount instance."),
-    UPGRADE_ACCOUNT_ROLE("Upgrades a UserAccount with the restricted role type to one with the Employee role type."),
-    DELETE_USER("Removes a UserAccount from the system."),
-    VIEW_ALL_REIMBURSEMENT_REQUESTS("template description"),
+    EDIT_USER_ROLE("Edit the Role attribute value of a UserAccount instance."),
+    UPGRADE_ACCOUNT_ROLE("Upgrade a UserAccount with the restricted role type to one with the Employee role type."),
+    DELETE_USER("Remove a UserAccount from the system."),
+    VIEW_ALL_REIMBURSEMENT_REQUESTS("View all reimbursement requests."),
+    VIEW_ALL_USERACCOUNTS("View all UserAccounts in the system."),
 
 
 
@@ -31,20 +32,22 @@ public enum Permissions {
      */
     CREATE_REIMBURSEMENT_REQUEST("template description"),
     VIEW_SUBMITTED_REIMBURSEMENT_REQUESTS("template description"),
-    LOGOUT("template description"),
+    VIEW_SINGLE_REIMBURSEMENT_REQUEST("template description"),
+    EDIT_PENDING_REIMBURSEMENT("template description"),
 
 
     /**
      * Restricted level:
      */
     REQUEST_EMPLOYEE_ACCOUNT("Requests an upgrade from Restricted role to Employee role."),
+    LOGOUT("Ends the user session and exits the system."),
 
 
     /**
      * Guest level:
      */
-    CREATE_ACCOUNT("Creates a new account with the default restricted role type and permissions.");
-
+    CREATE_ACCOUNT("Creates a new account with the default restricted role type and permissions."),
+    LOGIN("Attempts to authenticate the user and access the role based system.");
 
     private final String description;
 
