63f470bfdbd77b8277c37813abde3128b1d08bca
Edward J <edd.f.johnson.work@gmail.com>
2025-09-24 08:39:32 -0400

    impl: added definition for InvalidPasswordException

--- backend/src/main/java/com/ddjproj/revature/exception/security/InvalidPasswordException.java
+++ backend/src/main/java/com/ddjproj/revature/exception/security/InvalidPasswordException.java
@@ -1,4 +1,9 @@
 package com.ddjproj.revature.exception.security;
 
-public class InvalidPasswordException {
+import com.ddjproj.revature.exception.ApplicationException;
+
+public class InvalidPasswordException extends ApplicationException {
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
 }
