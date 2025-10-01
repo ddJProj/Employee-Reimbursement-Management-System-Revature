7c5a45baf1e99f6f05c372c05ced3b87099d26d3
Edward J <edd.f.johnson.work@gmail.com>
2025-10-01 13:27:39 -0400

    impl: basic button, placeholder

---
+++ frontend/src/components/layout/Button.tsx
@@ -0,0 +1,14 @@
+/**
+ * @file src/components/layout/.jsx
+ * @description 
+ * @module 
+ * 
+ * Resources:
+ * @see {@link } - 
+ */
+
+
+
+function Button({title} {title: string} {
+	return <button> {title}</button>
+)};
