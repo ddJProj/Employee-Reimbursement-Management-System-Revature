f2aa2e99cb0788468a98dafae8557514ef4f1011
Edward J <edd.f.johnson.work@gmail.com>
2025-10-01 13:27:11 -0400

    impl: basic header, placeholder

---
+++ frontend/src/components/layout/Header.jsx
@@ -0,0 +1,17 @@
+/**
+ * @file src/components/layout/.jsx
+ * @description 
+ * @module 
+ * 
+ * Resources:
+ * @see {@link } - 
+
+ */
+
+import React from "react";
+
+function Header(){
+	return <header><h1>Page Header</h1></header>
+}
+
+export default Header;
