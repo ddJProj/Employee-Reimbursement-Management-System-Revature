f7d702e115b9035c75c7f12692ad7c9b65c2bc07
Edward J <edd.f.johnson.work@gmail.com>
2025-10-02 10:14:58 -0400

    refactor: corrected file imports, and removed unused methods

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -10,6 +10,7 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
+
 // short hook to provide authContext 
 export function useAuth(){
   const context = useContext(AuthContext);
